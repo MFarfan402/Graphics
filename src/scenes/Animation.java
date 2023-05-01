64446a6ce369554c1e015e9dced1f0850ed0f1c6
Mauricio Farfan <maufar402@gmail.com>
2023-05-01 02:21:57 -0600

    Savepoint 2

--- src/scenes/Animation.java
+++ src/scenes/Animation.java
@@ -20,6 +20,7 @@ public class Animation extends JFrame implements Runnable
     private Thread thread;
 
     private PointXY planePoint;
+    private boolean leftDirection;
 
     public Animation()
     {
@@ -43,8 +44,8 @@ public class Animation extends JFrame implements Runnable
     @Override
     public void run() {
 
+        leftDirection = false;
         int limitX = planePoint.x + 40;
-        int limitY = planePoint.y + 200;
         for (int i = planePoint.x; i < limitX; i++)
         {
             try {
@@ -79,43 +80,107 @@ public class Animation extends JFrame implements Runnable
             }
             catch (InterruptedException e) {}
         }
+
+        leftDirection = true;
+        planePoint = new PointXY(500, 420);
+        for (int i = planePoint.x; i > -80; i--)
+        {
+            try
+            {
+                repaint();           planePoint.x--;
+                thread.sleep(30);
+            }
+            catch (InterruptedException e) {}
+        }
     }
 
     public void paint(Graphics g)
     {
         super.paint(graphics);
 
-
         PaintClouds();
         PaintPlane();
-
-
 
         g.drawImage(buffer, 0,0, this);
         g.dispose();
     }
     public void PaintPlane()
     {
-//        fillRectangle(planePoint, new PointXY(planePoint.x + 60, planePoint.y + 22), MyColor.plane);
-//        fillRectangle(new PointXY(planePoint.x, planePoint.y + 15), new PointXY(planePoint.x + 60, planePoint.y + 19), MyColor.planeBlue);
+        fillRectangle(planePoint, new PointXY(planePoint.x + 60, planePoint.y + 22), MyColor.plane);
+        fillRectangle(new PointXY(planePoint.x, planePoint.y + 15), new PointXY(planePoint.x + 60, planePoint.y + 19), MyColor.planeBlue);
 
-        drawTriangle(
-                new PointXY(planePoint.x + 60, planePoint.y),
-                new PointXY(planePoint.x + 80, planePoint.y + 10),
-                new PointXY(planePoint.x + 60, planePoint.y + 20),
-                MyColor.plane
-        );
+        if (leftDirection)
+        {
+            drawTriangle(
+                    new PointXY(planePoint.x, planePoint.y),
+                    new PointXY(planePoint.x - 20, planePoint.y + 10),
+                    new PointXY(planePoint.x, planePoint.y + 20),
+                    MyColor.plane
+            );
 
+            if ((planePoint.x - 5) > 0)
+                Rellenar(planePoint.x - 5, planePoint.y + 5, MyColor.plane, MyColor.plane);
 
-        Rellenar(planePoint.x + 65, planePoint.y + 5, MyColor.plane, MyColor.plane);
+            ArrayList<PointXY> blueTail = new ArrayList<PointXY>();
+            blueTail.add(new PointXY(planePoint.x + 60, planePoint.y));
+            blueTail.add(new PointXY(planePoint.x + 70, planePoint.y));
+            blueTail.add(new PointXY(planePoint.x + 77, planePoint.y + 10));
+            blueTail.add(new PointXY(planePoint.x + 70, planePoint.y + 20));
+            blueTail.add(new PointXY(planePoint.x + 60, planePoint.y + 20));
+            drawPolygon(blueTail, MyColor.planeBlue);
+            if (((planePoint.x+75) < 500) && ((planePoint.x+75) > 0))
+               Rellenar(planePoint.x + 62, planePoint.y + 2, MyColor.planeBlue, MyColor.planeBlue);
 
+            ArrayList<PointXY> tail = new ArrayList<PointXY>();
+            tail.add(new PointXY(planePoint.x + 60, planePoint.y));
+            tail.add(new PointXY(planePoint.x + 70, planePoint.y - 20));
+            tail.add(new PointXY(planePoint.x + 80, planePoint.y - 20));
+            tail.add(new PointXY(planePoint.x + 70, planePoint.y));
 
-//        fillRectangle(new PointXY(planePoint.x + 10, planePoint.y + 5), new PointXY(planePoint.x + 15, planePoint.y + 19), MyColor.windowPlane);
-//        fillRectangle(new PointXY(planePoint.x + 50, planePoint.y + 5), new PointXY(planePoint.x + 55, planePoint.y + 19), MyColor.windowPlane);
-//
-//        for (int i = 18; i < 40; i+=7)
-//            fillRectangle(new PointXY(planePoint.x + i, planePoint.y + 5), new PointXY(planePoint.x + i + 5, planePoint.y + 12), MyColor.windowPlane);
+            drawPolygon(tail, MyColor.plane);
+            PutPixel(planePoint.x + 65, planePoint.y - 5, Color.RED);
+            if ((planePoint.x + 75) < 500 && ((planePoint.x+75) > 0))
+                Rellenar(planePoint.x + 65, planePoint.y - 5, MyColor.plane, MyColor.plane);
+        }
+        else
+        {
+            drawTriangle(
+                    new PointXY(planePoint.x + 60, planePoint.y),
+                    new PointXY(planePoint.x + 80, planePoint.y + 10),
+                    new PointXY(planePoint.x + 60, planePoint.y + 20),
+                    MyColor.plane
+            );
 
+            if ((planePoint.x + 65) < 500)
+                Rellenar(planePoint.x + 65, planePoint.y + 5, MyColor.plane, MyColor.plane);
+
+            ArrayList<PointXY> blueTail = new ArrayList<PointXY>();
+            blueTail.add(new PointXY(planePoint.x, planePoint.y));
+            blueTail.add(new PointXY(planePoint.x - 10, planePoint.y));
+            blueTail.add(new PointXY(planePoint.x - 17, planePoint.y + 10));
+            blueTail.add(new PointXY(planePoint.x - 10, planePoint.y + 20));
+            blueTail.add(new PointXY(planePoint.x, planePoint.y + 20));
+
+            drawPolygon(blueTail, MyColor.planeBlue);
+            if ((planePoint.x) < 500)
+                Rellenar(planePoint.x - 5, planePoint.y + 5, MyColor.planeBlue, MyColor.planeBlue);
+
+            ArrayList<PointXY> tail = new ArrayList<PointXY>();
+            tail.add(new PointXY(planePoint.x, planePoint.y));
+            tail.add(new PointXY(planePoint.x - 10, planePoint.y - 20));
+            tail.add(new PointXY(planePoint.x - 20, planePoint.y - 20));
+            tail.add(new PointXY(planePoint.x - 10, planePoint.y));
+
+            drawPolygon(tail, MyColor.plane);
+            if ((planePoint.x)< 500)
+                Rellenar(planePoint.x - 5, planePoint.y - 5, MyColor.plane, MyColor.plane);
+        }
+
+        fillRectangle(new PointXY(planePoint.x + 10, planePoint.y + 5), new PointXY(planePoint.x + 15, planePoint.y + 19), MyColor.windowPlane);
+        fillRectangle(new PointXY(planePoint.x + 50, planePoint.y + 5), new PointXY(planePoint.x + 55, planePoint.y + 19), MyColor.windowPlane);
+
+        for (int i = 18; i < 40; i+=7)
+            fillRectangle(new PointXY(planePoint.x + i, planePoint.y + 5), new PointXY(planePoint.x + i + 5, planePoint.y + 12), MyColor.windowPlane);
 
     }
 
@@ -129,7 +194,7 @@ public class Animation extends JFrame implements Runnable
         return buffer.getRGB(x, y);
     }
 
-    public void fillCircle(PointXY center, int radius, Color fillColor) {
+  public void fillCircle(PointXY center, int radius, Color fillColor) {
         int x = center.x - radius;
         int y = center.y - radius;
         int diameter = radius * 2;
@@ -139,7 +204,8 @@ public class Animation extends JFrame implements Runnable
             int left = (int) Math.round(center.x - Math.sqrt(radius * radius - (row - center.y) * (row - center.y)));
             int right = (int) Math.round(center.x + Math.sqrt(radius * radius - (row - center.y) * (row - center.y)));
             for (int col = left; col <= right; col++)
-                PutPixel(col, row, fillColor);        }
+                PutPixel(col, row, fillColor);
+        }
     }
     public void fillRectangle(PointXY pointA, PointXY pointB, Color fillColor)
     {
@@ -175,7 +241,7 @@ public class Animation extends JFrame implements Runnable
         }
 
         for (int i = limiteIzquierdo; i <= limiteDerecho; i++) {
-            PutPixel(i, y, color);
+                PutPixel(i, y, color);
         }
 
         for (int i = limiteIzquierdo; i <= limiteDerecho; i++) {
@@ -190,9 +256,11 @@ public class Animation extends JFrame implements Runnable
     public void drawPolygon(ArrayList<PointXY> points, Color fillColor)
     {
         ArrayList<PointXY> newPoints = new ArrayList<PointXY>();
-        for (int i = 0; i < points.size() - 1; i++)
+        for (int i = 0; i < points.size(); i++)
         {
-            Line line = new Line(points.get(i), points.get(i+1));
+            Line line = (i == points.size() - 1) ?
+                new Line(points.get(i), points.get(0)):
+                new Line(points.get(i), points.get(i+1));
             newPoints.addAll(line.LineToPointsXY());
         }
 
@@ -221,6 +289,22 @@ public class Animation extends JFrame implements Runnable
         ArrayList<PointXY> points2 = line.LineToPointsXY();
         for (int i = 0; i < points2.size(); i++)
             PutPixel(points2.get(i).x, points2.get(i).y, MyColor.plane);
+
+        line = new Line(new PointXY(0, 0), new PointXY(0, 500));
+        points2 = line.LineToPointsXY();
+        for (int i = 0; i < points2.size(); i++)
+            PutPixel(points2.get(i).x, points2.get(i).y, MyColor.plane);
+
+//
+//        line = new Line(new PointXY(500, 0), new PointXY(500, 500));
+//        points2 = line.LineToPointsXY();
+//        for (int i = 0; i < points2.size(); i++)
+//            PutPixel(points2.get(i).x, points2.get(i).y, MyColor.planeBlue);
+
+//        line = new Line(new PointXY(500, 400), new PointXY(500, 500));
+//        points2 = line.LineToPointsXY();
+//        for (int i = 0; i < points2.size(); i++)
+//            PutPixel(points2.get(i).x, points2.get(i).y, MyColor.planeBlue);
     }
     public void PaintClouds()    {
         for (int y = 0; y < buffer.getHeight() - 1; y++)
@@ -229,5 +313,10 @@ public class Animation extends JFrame implements Runnable
         PaintClouds(Cloud.FirstCloud());
         PaintClouds(Cloud.SecondCloud());
         PaintClouds(Cloud.ThirdCloud());
+    }
+
+    public void SetBounds()
+    {
+
     }
 }
